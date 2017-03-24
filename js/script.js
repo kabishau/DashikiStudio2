dd2ab3d474509239a861e79e7ab9d4ee99179837
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-03-23 23:06:40 -0400

    Update

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function calculateTotalDistance(time, sheed1, speed2) {
-    var dist1 = speed1 * time;
-    var dist2 = speed2 * time;
-    var totalDist = dist1 + dist2;
-    alert(totalDist);
-}
-
-(calculateTotalDistance(2, 5, 5)
